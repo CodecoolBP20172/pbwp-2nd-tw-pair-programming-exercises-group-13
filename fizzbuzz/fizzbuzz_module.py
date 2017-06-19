4dee2c93cd7392a6c4fd73fcf38d06a1daf5e819
Debora Csomos <csomidebi@gmail.com>
2017-06-19 13:21:39 +0200

    solution for the fizzbuzz iteration

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,18 @@
 def fizzbuzz(number):
-    return
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
 
 
 def main():
-    return
+    for i in range(1, 101):
+        fizzbuzz(i)
+
 
 if __name__ == '__main__':
     main()
